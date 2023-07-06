b46401c7ffb4b3e4feee9a1ddfa4419f73595811
Vincent Messika <110119058+VincentM1978@users.noreply.github.com>
2023-07-06 11:10:24 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,63 @@
+import pandas as pd
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.express as px
+
+###################################################### importation et nettoyage du csv #########################################################
+
+
+df = pd.read_csv('https://raw.githubusercontent.com/chriszapp/datasets/main/books.csv', sep=',', on_bad_lines='warn')
+df['publication_date'] = pd.to_datetime(df['publication_date'], errors='coerce')
+df['year'] = df['publication_date'].dt.year
+df['year'] = pd.to_datetime(df['year'], format='%Y').dt.year
+df['year'] = df['year'].astype(float).astype(pd.Int64Dtype())
+
+
+###################################################### creation de l'application #########################################################
+
+# Create the Dash app
+app = dash.Dash(__name__)
+
+############################################################# creation du layout #############################################################
+
+# Define the layout of the app
+app.layout = html.Div([
+    html.H1("Book Dataset Analysis"),
+    
+    # Dropdown for variable 1
+    dcc.Dropdown(
+        id='variable1',
+        options=[{'label': col, 'value': col} for col in df.columns],
+        value='average_rating',
+        clearable=False
+    ),
+    
+    # Dropdown for variable 2
+    dcc.Dropdown(
+        id='variable2',
+        options=[{'label': col, 'value': col} for col in df.columns],
+        value='num_pages',
+        clearable=False
+    ),
+    
+    # Graph
+    dcc.Graph(id='plot'),
+])
+
+
+###################################################### creation des callbacks #########################################################
+
+# Define the callback to update the plot
+@app.callback(
+    dash.dependencies.Output('plot', 'figure'),
+    dash.dependencies.Input('variable1', 'value'),
+    dash.dependencies.Input('variable2', 'value')
+)
+def update_plot(variable1, variable2):
+    fig = px.scatter(df, x=variable1, y=variable2, hover_data=['title'])
+    return fig
+
+# Run the app
+if __name__ == '__main__':
+    app.run_server(debug=True)
